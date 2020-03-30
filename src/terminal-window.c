6963062e1a84133f70d1296b75d2ba2f7b861ce0
rbuj <robert.buj@gmail.com>
2020-03-30 22:51:18 +0200

    terminal-window: expand the content of the comment on about dialog

--- src/terminal-window.c
+++ src/terminal-window.c
@@ -4463,7 +4463,7 @@ help_about_callback (GtkAction *action,
 
     licence_text = terminal_util_get_licence_text ();
 
-    comments = g_strdup_printf (_("A terminal emulator for the MATE desktop\nUsing VTE %d.%d.%d"),
+    comments = g_strdup_printf (_("It's a terminal emulator for the MATE Desktop Environment. MATE Terminal is a fork of GNOME Terminal written by Havoc Pennington and others. MATE Terminal is using VTE %d.%d.%d, which provides a virtual terminal widget for GTK applications."),
                                 vte_get_major_version (), vte_get_minor_version (), vte_get_micro_version ());
 
     gtk_show_about_dialog (GTK_WINDOW (window),
